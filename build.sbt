99cede5c31ee29379622e54b5db7a937747f259c
Andy Muir <andy.muir@sky.uk>
2018-12-19 16:43:08 +0000

    Stream directly from the incoming.op.msgs topic to the switch.modification.instructions topic. Replace incoming String with hard coded XML for the switch!

--- build.sbt
+++ build.sbt
@@ -3,3 +3,10 @@ name := "sns-knitware-converter"
 version := "0.1"
 
 scalaVersion := "2.12.8"
+
+libraryDependencies += "org.apache.kafka" %% "kafka-streams-scala" % "2.0.0"
+
+lazy val testDependencies = Seq(
+  "org.scalatest"              %% "scalatest"      % "3.0.5" % "test",
+  "com.madewithtea"            %% "mockedstreams"  % "1.6.0" % "test"
+)
---
+++ src/main/scala/sns/lando/knitware/converter/KnitwareConverter.scala
@@ -0,0 +1,67 @@
+package sns.lando.knitware.converter
+
+import java.util.Properties
+import java.util.concurrent.TimeUnit
+
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.streams.scala.ImplicitConversions._
+import org.apache.kafka.streams.scala.Serdes._
+import org.apache.kafka.streams.scala.StreamsBuilder
+import org.apache.kafka.streams.scala.kstream.KStream
+import org.apache.kafka.streams.{KafkaStreams, StreamsConfig}
+
+object KnitwareConverter extends App {
+
+
+  private val lluStreamMessagesTopic = "incoming.op.msgs"
+  private val switchModificationTopic = "switch.modification.instructions"
+  private val KafkaDeserializer = "org.apache.kafka.common.serialization.StringDeserializer"
+  private val KafkaSerializer = "org.apache.kafka.common.serialization.StringSerializer"
+
+  private val props = new Properties()
+
+  props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
+  props.put("acks", "all")
+  props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, KafkaSerializer)
+  props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaSerializer)
+  props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaDeserializer)
+  props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, KafkaDeserializer)
+  props.put(ConsumerConfig.GROUP_ID_CONFIG, this.getClass.getName)
+
+  props.put(StreamsConfig.APPLICATION_ID_CONFIG, "sns-knitware-converter")
+  props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
+
+
+  val builder: StreamsBuilder = new StreamsBuilder
+  val textLines: KStream[String, String] = builder.stream[String, String](lluStreamMessagesTopic)
+
+  def getXmlFor(textLine: String): String =
+    """
+      |<?xml version="1.0" encoding="UTF-8"?>
+      |<switchServiceModificationInstruction switchServiceId="16" netstreamCorrelationId="33269793">
+      |  <features>
+      |    <callerDisplay active="true"/>
+      |    <ringBack active="true"/>
+      |    <chooseToRefuse active="true"/>
+      |  </features>
+      |</switchServiceModificationInstruction>
+    """.stripMargin
+
+
+  textLines.mapValues(textLine => getXmlFor(textLine))
+    .to(switchModificationTopic)
+  //  val wordCounts: KTable[String, Long] = textLines
+  //    .flatMapValues(textLine => textLine.toLowerCase.split("\\W+"))
+  //    .groupBy((_, word) => word)
+  //    .count(Materialized.as("counts-store"))
+  //  wordCounts.toStream.to("WordsWithCountsTopic")
+
+  val streams: KafkaStreams = new KafkaStreams(builder.build(), props)
+  streams.start()
+
+  sys.ShutdownHookThread {
+    streams.close(10, TimeUnit.SECONDS)
+  }
+
+}
